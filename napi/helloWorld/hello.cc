959f9541a313f3d97a0d10483b571b117f3c1dc8
lixiaowei <promise9495@163.com>
2021-05-20 16:52:51 +0800

    添加注释

--- napi/helloWorld/hello.cc
+++ napi/helloWorld/hello.cc
@@ -6,22 +6,31 @@ napi_value Method(napi_env env, napi_callback_info args)
 {
   napi_value greeting;
   napi_status status;
-  std::cout << *env << std::endl;
+  std::cout << env << std::endl;
   status = napi_create_string_utf8(env, "word", NAPI_AUTO_LENGTH, &greeting);
   if (status != napi_ok)
     return nullptr;
   return greeting;
 }
 
+/**
+ * @brief 初始化函数
+ * 
+ * @param env 
+ * @param exports 
+ * @return napi_value 
+ */
 napi_value init(napi_env env, napi_value exports)
 {
   napi_status status;
   napi_value fn;
 
+  // 创建函数fn，从Method转义
   status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
   if (status != napi_ok)
     return nullptr;
 
+  // 添加hello函数，函数体为fn
   status = napi_set_named_property(env, exports, "hello", fn);
   if (status != napi_ok)
   {
@@ -31,4 +40,6 @@ napi_value init(napi_env env, napi_value exports)
   return exports;
 }
 
+// 后面不能添加分号，不是一个函数
+// node-gyp构建时使用 NODE_GYP_MODULE_NAME 宏来定义module名称
 NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
